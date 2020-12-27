b1139626c3026936fe91a6fef9028fcafbb3b6b7
0phone <zhiguan7@gmail.com>
2020-12-27 15:35:45 +0800

    修正用户行为

--- src/main/java/com/wyu/dao/UserDao.java
+++ src/main/java/com/wyu/dao/UserDao.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface UserDao extends JpaRepository<User,Long> {
 
-    @Query("select * from user where user_name = ?1")
+    @Query("select u from User u where u.user_name = ?1")
     User findByUserName(String name);
 
 }
