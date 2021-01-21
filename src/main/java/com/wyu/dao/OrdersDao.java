17d4da09f27ca97e8fa6b0e0c345847959efc777
0phone <zhiguan7@gmail.com>
2021-01-21 10:52:56 +0800

    修改订单项

--- src/main/java/com/wyu/dao/OrdersDao.java
+++ src/main/java/com/wyu/dao/OrdersDao.java
@@ -13,6 +13,6 @@ public interface OrdersDao extends JpaRepository<Orders,Long> {
 
     @Modifying
     @Transactional
-    @Query("update Order o set o.payment = :#{#payment} where o.orders_id = :#{#id}")
+    @Query("update Orders o set o.payment = :#{#payment} where o.orders_id = :#{#id}")
     int setState(@Param("id")long id, @Param("payment") Orders.Payment payment);
 }
